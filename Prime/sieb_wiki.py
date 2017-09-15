fde3ece130604675e6d63791e774c53d9e44149a
DerHirschi <hirschi@saw-emusic.de>
2017-09-15 21:06:11 +0200

    Added sieb_wiki.py

---
+++ Prime/sieb_wiki.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Quelle : https://de.wikibooks.org/wiki/Primzahlen:_Programmbeispiele
+import time
+
+def main():
+
+    obergrenze = 100000000
+    # Jede Zahl zwischen 1 und obergrenze wird zuerst als prim angenommen
+    zahlen = [True] * (obergrenze + 1)
+    # Da 0 und 1 keine Primzahlen sind, werden sie auf False gesetzt
+    zahlen[0] = False
+    zahlen[1] = False
+
+    i = 2
+    ti = time.time()
+
+    while i * i <= obergrenze:
+        if zahlen[i] == True:  # Die Zahl i ist als prim markiert
+            # Streiche alle Vielfachen von i
+            for k in range(i * i, obergrenze + 1, i):
+                zahlen[k] = False
+
+        i = i + 1
+
+    # Ausgabe aller gefundenen Zahlen
+    for i, v in enumerate(zahlen):
+        if v == True:
+            print i, 'ist prim.'
+
+    print time.time() -ti
+    return 0
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Some Math Scripts
 #- Prime
 -   Sieb des Eratosthenes - sieb_v*.py
+-   Beispiel von WIKIBOOKS - sieb_wiki.py
 
 #- Fibonacci
 -   Fibonacci Rechner - fibu_count.py
 
 #- Sort
--   simple Bubble sort - Bubble.py
+-   simple Bubble sort - bubble.py
 
